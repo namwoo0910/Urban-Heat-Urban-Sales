d795573d75ac1cccda2be4af012c8cb19be42743
Seokwoo <ysu1516@gmail.com>
2025-09-22 02:31:33 +0900

    [fix] #24 - mapbox error

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,23 +22,9 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.className}>
       <head>
-        {/* Preload critical resources for faster particle layer loading */}
-        <link 
-          rel="preload" 
-          href="/seoul_boundary.geojson" 
-          as="fetch" 
-          crossOrigin="anonymous"
-          type="application/json"
-        />
-        {/* Preload Mapbox CSS */}
-        <link 
-          rel="preload" 
-          href="https://api.mapbox.com/mapbox-gl-js/v2.15.0/mapbox-gl.css" 
-          as="style"
-        />
-        {/* DNS prefetch for external resources */}
-        <link rel="dns-prefetch" href="//api.mapbox.com" />
-        <link rel="dns-prefetch" href="//tiles.mapbox.com" />
+        {/* Connection hints for Mapbox tiles */}
+        <link rel="preconnect" href="https://api.mapbox.com" crossOrigin="anonymous" />
+        <link rel="preconnect" href="https://tiles.mapbox.com" crossOrigin="anonymous" />
       </head>
       <body className="bg-[#0a0a0a] text-white">
         <GsapProvider>
